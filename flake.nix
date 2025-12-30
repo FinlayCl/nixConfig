504b3c686e135f1ca6da62d13c99df1d1b4cdfe9
Finlay <fclements1@sheffield.ac.uk>
2025-12-30 17:19:21 +0000

    initial setup for nixos with home manager

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+
+  description = "My flake!";
+
+  inputs = {
+     nixpkgs.url = "nixpkgs/master";
+     home-manager.url = "github:nix-community/home-manager/master";
+     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+	modules = [ ./configuration.nix ];
+	};
+    };
+    homeConfigurations = {
+      finlay = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home.nix ];
+      };
+    };
+  };
+}
