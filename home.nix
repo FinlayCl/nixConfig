87ad7a465cba5e6eb161336b094b2c914a9ed80f
Finlay <fclements1@sheffield.ac.uk>
2025-12-31 15:35:32 +0000

    niri

--- home.nix
+++ home.nix
@@ -13,6 +13,9 @@
     settings.init.defaultBranch = "main";
     settings.safe.directory = "/etc/nixos";
   };
+  
+  programs.niri.enable = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
